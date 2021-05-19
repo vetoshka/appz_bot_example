a5ed9fd2a4516f1066278de7ad93ed7a72ef4ca3
Ann Kolomiets <43034693+vetoshka@users.noreply.github.com>
2021-05-19 11:27:43 +0300

    Update HelloWorldBot.java

--- hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
+++ hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
@@ -6,6 +6,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 public final class HelloWorldBot extends Bot {
     public static void main(String[] args){
+        //
         if(args == null || args.length != 2){
             System.out.println("You must run bot with 2 args - BotToken and bot UserName");
         } else {
